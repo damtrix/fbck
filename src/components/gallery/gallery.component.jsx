3b4133ddad56d0ada3ae41b7d16f0377c57dbaaf
spiritbuilder <olaifatimothy@gmail.com>
2021-09-17 12:45:46 +0100

    fix gallery

--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import React,{useState} from 'react';
+import {ChevronLeft, ChevronRight} from "react-feather"
+
 import './gallery.styles.scss'
+
  
-const slideImages = [
-  'https://res.cloudinary.com/damtrix/image/upload/homePage/IMG-20200628-WA0020_jmsikb.jpg',
-  'https://res.cloudinary.com/damtrix/image/upload/homePage/building1_ruzxyk.jpg',
-  'https://res.cloudinary.com/damtrix/image/upload/v1593809734/homePage/IMG-20200628-WA0018_xaagvp.jpg'
+
+const slideImage = [
+'https://res.cloudinary.com/damtrix/image/upload/homePage/IMG-20200628-WA0020_jmsikb.jpg',
+'https://res.cloudinary.com/damtrix/image/upload/homePage/building1_ruzxyk.jpg'
 ];
+
+// 'https://res.cloudinary.com/damtrix/image/upload/v1593809734/homePage/IMG-20200628-WA0018_xaagvp.jpg'
  
 const Gallery = () => {
-  return (
+  const [showing, setShowing] = useState(0)
+const move=(k)=>{
+  if(k==="left"){
+    if(showing===0){
+      setShowing(1)
+    }
+  }else{
+    if(showing===1){
+      setShowing(0)
+    }
+  }
+}
+  setTimeout(() => {
+   if(showing===1){
+     setShowing(0)
+   } else{
+     setShowing(showing+1)
+   }
+  }, 2200);
+    return(
     <div className="container">
-      <Slide easing="ease">
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-            <span>Slide 1</span>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-            <span>Slide 2</span>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-            <span>Slide 3</span>
-          </div>
-        </div>
-      </Slide>
+
+<img src={slideImage[showing]} className="image" />
+<div onClick={()=>{
+  move("left")
+}} className="left-arrow"><ChevronLeft color="black" size={48}/></div>
+<div onClick={()=>{
+  move("right")
+}} className="right-arrow"><ChevronRight color="black" size={48}/></div>
+<div className="circleholder">
+<div onClick={()=>{
+  setShowing(0)
+}} on className={showing===0?"showing":"notshowing"}></div>
+<div onClick={()=>{
+  setShowing(1)
+}} className={showing===1?"showing":"notshowing"}></div>
+{/* <div className={showing===2?"showing":"notshowing"}></div> */}
+</div>
     </div>
-  )
+    )
+    
+  
 };
 
 export default Gallery
